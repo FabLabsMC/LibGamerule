eeab8b571b98e00e01bea755df902a5e3e331037
Martmists <martmists@gmail.com>
2020-02-24 13:16:47 +0100

    Use Getter interface

--- src/main/java/com/martmists/libgamerule/entities/ClientHook.java
+++ src/main/java/com/martmists/libgamerule/entities/ClientHook.java
@@ -1,11 +1,10 @@
 package com.martmists.libgamerule.entities;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.MinecraftServer;
 
 public class ClientHook {
     public static MinecraftServer getClientServer(){
-        return ((MinecraftClient) FabricLoader.getInstance().getGameInstance()).getServer();
+        return ((ServerGetter) FabricLoader.getInstance().getGameInstance()).getServer();
     }
 }
---
+++ src/main/java/com/martmists/libgamerule/entities/ServerGetter.java
@@ -0,0 +1,7 @@
+package com.martmists.libgamerule.entities;
+
+import net.minecraft.server.MinecraftServer;
+
+public interface ServerGetter {
+    MinecraftServer getServer();
+}
